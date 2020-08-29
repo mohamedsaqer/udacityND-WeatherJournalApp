82f8cbf40d59036b0c9f3ad0adc22bf25cb66dfe
Mohamed Saqer <mohamed.saqer@outlook.com>
2020-08-29 18:12:10 +0200

    App.js

--- website/app.js
+++ website/app.js
@@ -1,4 +1,21 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey = '1a03b8d2530728b02e7dab283b20c3da';
+
+// Event listener to add function to existing HTML DOM element
+document.querySelector('#generate').addEventListener('click', generateTemp)
+
+/* Function called by event listener */
+function generateTemp()
+{
+    console.log('dksaghk');
+}
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+/* Function to GET Project Data */
 
 // Create a new date instance dynamically with JS
 let d = new Date();
