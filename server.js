b7598ee2b68b060360043cc8a1de570dcaa0cccb
Mohamed Saqer <mohamed.saqer@outlook.com>
2020-08-29 17:46:30 +0200

    server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+// projectData = {};
+projectData = {name:'saqer',age:'2050'};
 
 // Require Express to run server and routes
 const express = require('express');
@@ -7,9 +8,10 @@ const express = require('express');
 // Start up an instance of app
 const app = express();
 
+/* Dependencies */
+const bodyParser = require('body-parser')
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -24,6 +26,23 @@ const port = 3000;
 
 // Setup Server
 app.listen(port, listening)
+
+// Callback to debug
 function listening() {
     console.log(`running on localhost: ${port}`);
 };
+
+// Initialize all route with a callback function
+app.get('/all', sendData);
+
+// Callback function to complete GET '/all'
+function sendData(request, response) {
+    response.send(projectData);
+};
+
+// POST route
+app.post('/add', callBack);
+
+function callBack(request, response) {
+    response.send('POST received');
+}
